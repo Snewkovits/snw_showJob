ce50558b06020a5c4e28d79af3a063e402eb2b8a
Snewkovits <95080058+Snewkovits@users.noreply.github.com>
2022-02-15 13:18:46 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -7,7 +7,3 @@ description 'Showing the players job name and grade name'
 client_script 'main/client.lua'
 server_script 'main/server.lua'
 shared_script 'config.lua'
-
-dependencies {
-  'es_extended'
-}
