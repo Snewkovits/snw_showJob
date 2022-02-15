e9a22a97e2aa4c10120aefe776d7884ab51fcaf1
Snewkovits <95080058+Snewkovits@users.noreply.github.com>
2022-02-15 13:17:27 +0100

    Update client.lua
    The script has updateded to multi framework. It can work with ESX or QBCore.

--- main/client.lua
+++ main/client.lua
@@ -3,23 +3,33 @@ local PlayerData = {}
 local jobName = nil
 
 Citizen.CreateThread(function()
-    while ESX == nil do
-        TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
-        Citizen.Wait(0)
-    end
-    if ESX == nil then
-        TriggerServerEvent('snw_showJobs:badState', "^5[SNW_SHOWJOB]^0 Script ^1ERROR^0")
-    else
-        TriggerServerEvent('snw_showJobs:goodState', "^5[SNW_SHOWJOB]^0 Script is started ^2Successfully^0")
-    end
-    if Config.debug == true then
-        if ESX == nil then
-            print("Can't load the ESX library")
-        else
-            print("ESX is succesfully loaded")
+    if Config.Framework == ESX then
+
+        while ESX == nil do
+            TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+            Citizen.Wait(0)
         end
+        if ESX == nil then
+            TriggerServerEvent('snw_showJobs:badState', "^5[SNW_SHOWJOB]^0 ESX ^1ERROR^0 at ID: "..tostring(PlayerId()))
+        end
+
+        local fost = ESX.GetPlayerData().job.label
+        local fost2 = ESX.GetPlayerData().job.grade_label
+
+        
+    end)
+elseif Config.Framework == QBCore then
+    local QBCore = exports['qb-core']:GetCoreObject()
+
+    if QBCore == nil then
+        TriggerServerEvent('snw_showJobs:badState', "^5[SNW_SHOWJOB]^0 QBCore ^1ERROR^0 at ID: ".. tostring(PlayerPedId()))
     end
-end)
+
+    local fost = QBCore.Functions.GetPlayerData.job.label
+    local fost2 = QBCore.Functions.GetPlayerData.job.grade_label
+else
+    TriggerServerEvent('snw_showJobs:badState', "^5[SNW:SHOWJOB]^1 ERROR AT FRAMEWORK NAME!^0")
+end
 
 local jobbb = false
 Citizen.CreateThread(function()
@@ -40,14 +50,12 @@ Citizen.CreateThread(function()
                 local scale = Config.text.scale
                 local font = Config.text.font
                 local outline = Config.text.outline
-                local fost = ESX.GetPlayerData().job.label
                     --SECOND LAYER
                 local posX2 = Config.text2.xpos
                 local posY2 = Config.text2.ypos
                 local scale2 = Config.text2.scale
                 local font2 = Config.text2.font
                 local outline = Config.text.outline
-                local fost2 = ESX.GetPlayerData().job.grade_label
                 if Config.debug == true then
                     print("Succesfully loaded")
                 end
@@ -58,14 +66,14 @@ Citizen.CreateThread(function()
                         SetTextFont(font)
                         SetTextOutline(outline)
                         BeginTextCommandDisplayText(string)
-                        AddTextComponentSubstringPlayerName(ESX.GetPlayerData().job.label)
+                        AddTextComponentSubstringPlayerName(fost)
                         EndTextCommandDisplayText(posX, posY)
                         --SECOND LAYER
                         SetTextScale(scale2, scale2)
                         SetTextFont(font2)
                         SetTextOutline(outline2)
                         BeginTextCommandDisplayText(string)
-                        AddTextComponentSubstringPlayerName(ESX.GetPlayerData().job.grade_label)
+                        AddTextComponentSubstringPlayerName(fost2)
                         EndTextCommandDisplayText(posX2, posY2)
                     end
                 end
